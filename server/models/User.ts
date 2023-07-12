88a9e56cbea4e5f40b071c34935efb70f3e6fb97
Yauheni Herasimenka <gerasik1992@gmail.com>
2023-07-13 02:34:08 +0300

    mover apollo to v3

--- server/models/User.ts
+++ server/models/User.ts
@@ -1,10 +1,31 @@
-import { model, Schema } from "mongoose"
+// import { model, Schema } from "mongoose"
 
-const userSchema = new Schema({
-  username: { type: String, default: null },
-  email: { type: String, uniq: true },
-  password: { type: String },
-  token: { type: String },
-})
+// const userSchema = new Schema({
+//   username: { type: String, default: null },
+//   email: { type: String, uniq: true },
+//   password: { type: String },
+//   token: { type: String },
+// })
 
-export default model("User", userSchema)
+// export default model("User", userSchema)
+
+import { MongoDataSource } from "apollo-datasource-mongodb"
+import { ObjectId } from "mongodb"
+
+interface UserDocument {
+  _id: ObjectId
+  username: string
+  email: string
+  password: string
+  token: string
+}
+
+// This is optional
+interface Context {
+  loggedInUser: UserDocument
+}
+export default class Users extends MongoDataSource<UserDocument, Context> {
+  getUser(userId: ObjectId) {
+    return this.findOneById(userId)
+  }
+}
